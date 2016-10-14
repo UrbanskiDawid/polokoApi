7f584adb18358c77d6e1dbaf91fae0b15ff6efd5
Dawid <git@dawidurbanski.pl>
2016-10-15 01:01:08 +0200

    search

--- polokoApi.py
+++ polokoApi.py
@@ -19,47 +19,74 @@ DIRECTION_FROM="FROM"
 
 class timetables:
 
-  #curl 'http://poloko.pl/polokoApi/timetables/agencies' -H 'Pragma: no-cache' -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: en,pl;q=0.8' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36' -H 'Accept: application/json, text/plain, */*' -H 'Referer: http://poloko.pl/' -H 'Cookie: lastSearched=%5B%7B%22id%22%3A%228-20%22%2C%22name%22%3A%2223%22%2C%22lastStop%22%3A%22Czerwone%20Maki%22%2C%22routeType%22%3A%22TRAM%22%2C%22direction%22%3A%22TO%22%2C%22agencyId%22%3Anull%2C%22resultType%22%3A%22route%22%7D%5D; lang=PL; _ga=GA1.2.1967863865.1476465638' -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' --compressed
+  #curl 'http://poloko.pl/polokoApi//timetables/search'
+  #-H 'Content-Type: application/json;charset=UTF-8'
+  #--data-binary '{"agencyIds":["mpk_krakow"],"searchString":"0"}'
+  @staticmethod
+  def searchAgency(agencyId,str):
+    return _getPost(
+      'search',
+      '{{"agencyIds":["{0}"],"searchString":"{1}"}}'.format(agencyId,str)
+    )#{u'routes':[], u'stops':[]}
+
+  #curl 'http://poloko.pl/polokoApi/timetables/agencies'
   @staticmethod
   def getAgencies():
     res = requests.get('http://poloko.pl/polokoApi/timetables/agencies')
     return res.json() #agencies [ {groupId,inconUrl,id,name}] groups [{iconUrl,id,name}]
 
-  #curl 'http://poloko.pl/polokoApi/timetables/routes' -H 'Pragma: no-cache' -H 'Origin: http://poloko.pl' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en,pl;q=0.8' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36' -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -H 'Cache-Control: no-cache' -H 'Referer: http://poloko.pl/' -H 'Cookie: lastSearched=%5B%7B%22id%22%3A%228-20%22%2C%22name%22%3A%2223%22%2C%22lastStop%22%3A%22Czerwone%20Maki%22%2C%22routeType%22%3A%22TRAM%22%2C%22direction%22%3A%22TO%22%2C%22agencyId%22%3Anull%2C%22resultType%22%3A%22route%22%7D%5D; _gat=1; hideCookiesMsg=true; _ga=GA1.2.1967863865.1476465638; lang=PL' -H 'Connection: keep-alive' -H 'DNT: 1' --data-binary '["mpk_krakow"]' --compressed
+  #curl 'http://poloko.pl/polokoApi/timetables/routes'
+  #-H 'Content-Type: application/json;charset=UTF-8'
+  #--data-binary '["mpk_krakow"]'
   @staticmethod
   def getRoutes(AgencieName):
     return _getPost(
       'routes',
       '["{0}"]'.format(AgencieName)
-      ) #{agentcyId,id,lastStop,name,routeType}
+      )
 
-  "#route '8-20' 'TO'"
   @staticmethod
   def getRoute(RouteID,Direction):
     return _getPost('route','{{ "routeId":"{0}","direction":"{1}" }}'.format(RouteID,Direction))
 
-  #getStop
+  #get stops?
+
   @staticmethod
   def getStop(stopID):
     return _getPost('getStop',stopID)
   
-  #TODO
-  @staticmethod
-  def getRouteForStopAndTrip(self,routeId,stopId,direction,tripId):
-    return _getPost(
-      'routeForStopAndTrip',
-      '{'+\
-	  ' "routeId":"{0}"'+\
-	  ',"stopId":"{1}"'+\
-	  ',"direction":"{2}"'+\
-	  ',"tripId":"{3}"'+\
-	  '}'.format(routeId,stopId,direction,tripId)
-	)
-
-  #get timetable #date = 10-10-2016
   @staticmethod
   def getTimetable(routeId,direction,stopId):
     date=time.strftime("%d-%m-%Y")
     return _getPost('timetable',
       '{{"routeId":"{0}","direction":"{1}","stopId":"{2}","fromDate":"{3}","toDate":"{4}"}}'.format(routeId,direction,stopId,date,date)
     )
+
+  #curl 'http://poloko.pl/polokoApi/timetables/departuresForRoute'
+  #-H 'Content-Type: application/json;charset=UTF-8' 
+  #--data-binary '{"stopId":"8-p033201B","routeId":"8-29","sinceTime":"14-10-2016 22:53"}'
+  @staticmethod
+  def getDeparturesForRoute(stopID,routeId):
+    Time=time.strftime("%d-%m-%Y %H:%M")
+    return _getPost('timetable','{{"stopId":"{0}","routeId":"{1}","sinceTime":"{2}"}}'.format(stopId,routeID,Time))
+
+#curl 'http://poloko.pl/polokoApi/timetables/routeForStop' 
+#-H 'Content-Type: application/json;charset=UTF-8' 
+#--data-binary '{"routeId":"8-29","stopId":"8-p033201B","direction":"TO"}'
+
+#curl 'http://poloko.pl/polokoApi/timetables/routeForStopAndTrip' 
+#-H 'Content-Type: application/json;charset=UTF-8' 
+#--data-binary '{"routeId":"8-0","stopId":"8-p012500T","direction":"FROM","tripId":"8-287"}'
+
+#curl 'http://poloko.pl/polokoApi/timetables/nearTransfers' 
+#-H 'Content-Type: application/json;charset=UTF-8' 
+#--data-binary '{"lat":50.05304703257761,"lng":19.91668939590454,"agencyIds":["mpk_krakow"]}'
+
+#curl 'http://poloko.pl/polokoService/departure' 
+#-H 'Content-Type: application/json;charset=UTF-8' 
+#--data-binary '{"dateTime":"14-10-2016 05:00","from":{"lat":"50.025711","lng":"19.920745"},"to":{"lat":"50.065245","lng":"19.877317"},"routingType":"OPTIMAL"}'
+
+
+#curl 'http://poloko.pl/polokoApi//timetables/search' 
+#-H 'Content-Type: application/json;charset=UTF-8' 
+#--data-binary '{"agencyIds":["mpk_krakow"],"searchString":"0"}'
